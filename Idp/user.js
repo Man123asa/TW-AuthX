81d00a2e7b74bd6288bb2a891c97c1e628b69da4
Manasa <sbethana@gitam.in>
2026-03-26 14:36:39 +0000

    Add User model with schema definition

--- Idp/user.js
+++ Idp/user.js
@@ -1,1 +1,8 @@
-
+const mongoose=require('mongoose');
+const UserSchema=new mongoose.Schema
+({
+    username:String,
+    publicKey:Object,
+    jkt:String
+});
+module.exports=mongoose.model('User',UserSchema);
